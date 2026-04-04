f3c9a09e1663100f5f99d5ad991d498a85a73066
Tran Gia Nguyen Phong <107380312+FongFox@users.noreply.github.com>
2026-04-04 12:03:49 +0000

    [LeetCode Sync] Runtime - 2 ms (99.33%), Memory - 44.5 MB (56.47%)

--- my-solutions/0125-valid-palindrome/solution.java
+++ my-solutions/0125-valid-palindrome/solution.java
@@ -1,20 +1,41 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        char[] temp = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase().toCharArray();
+        //Solution 1.0: traditonal for loop
+        char[] charArr = s.toCharArray();
+        int leftPtr = 0;
+        int rightPtr = charArr.length - 1;
 
-        if(temp.equals("")) {
-            return true;
-        }
+        while(leftPtr < rightPtr) {
+            // Convert it to ascii first
+            // Not in (!)
+            // (charArr[leftPtr] >= 48 && charArr[leftPtr] <= 57) 
+            // || (charArr[leftPtr] >= 65 && charArr[leftPtr] < =90)
+            // || (charArr[leftPtr] >= 97 && charArr[leftPtr] <= 122)
+            int asciiLeftChar = (int) charArr[leftPtr];
+            if(!((asciiLeftChar >= 48 && asciiLeftChar <= 57) || (asciiLeftChar >= 65 && asciiLeftChar <= 90) || (asciiLeftChar >= 97 && asciiLeftChar <= 122))) {
+                leftPtr++;
+                continue;
+            }
 
-        int leftIndex = 0;
-        int rightIndex = temp.length - 1;
+            // Convert it to ascii first
+            // Not in (!)
+            // (charArr[rightPtr] >= 48 && charArr[rightPtr] <= 57) 
+            // || (charArr[rightPtr] >= 65 && charArr[rightPtr] < =90)
+            // || (charArr[rightPtr] >= 97 && charArr[rightPtr] <= 122)
+            int asciiRightChar = (int) charArr[rightPtr];
+            if(!((asciiRightChar >= 48 && asciiRightChar <= 57) || (asciiRightChar >= 65 && asciiRightChar <= 90) || (asciiRightChar >= 97 && asciiRightChar <= 122))) {
+                rightPtr--;
+                continue;
+            }
 
-        while(leftIndex < rightIndex) {
-            if(temp[leftIndex] != temp[rightIndex]) {
+            char leftChar = Character.toLowerCase(charArr[leftPtr]);
+            char rightChar = Character.toLowerCase(charArr[rightPtr]);
+            if(leftChar != rightChar) {
                 return false;
             }
-            leftIndex++;
-            rightIndex--;
+
+            leftPtr++;
+            rightPtr--;
         }
 
         return true;
