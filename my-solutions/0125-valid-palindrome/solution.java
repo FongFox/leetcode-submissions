26c29a0bc4ac03b0ca19cc916e812e22e4074b67
Tran Gia Nguyen Phong <107380312+FongFox@users.noreply.github.com>
2026-04-11 10:59:37 +0000

    [LeetCode Sync] Runtime - 2 ms (99.33%), Memory - 44.4 MB (64.98%)

--- my-solutions/0125-valid-palindrome/solution.java
+++ my-solutions/0125-valid-palindrome/solution.java
@@ -1,41 +1,37 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //Solution 1.0: traditonal for loop
-        char[] charArr = s.toCharArray();
-        int leftPtr = 0;
-        int rightPtr = charArr.length - 1;
+        if(s.length() == 0) {
+            return false;
+        }
 
-        while(leftPtr < rightPtr) {
-            // Convert it to ascii first
-            // Not in (!)
-            // (charArr[leftPtr] >= 48 && charArr[leftPtr] <= 57) 
-            // || (charArr[leftPtr] >= 65 && charArr[leftPtr] < =90)
-            // || (charArr[leftPtr] >= 97 && charArr[leftPtr] <= 122)
-            int asciiLeftChar = (int) charArr[leftPtr];
-            if(!((asciiLeftChar >= 48 && asciiLeftChar <= 57) || (asciiLeftChar >= 65 && asciiLeftChar <= 90) || (asciiLeftChar >= 97 && asciiLeftChar <= 122))) {
-                leftPtr++;
+        char[] temp = s.toCharArray();
+        int leftIndex = 0;
+        int rightIndex = temp.length - 1;
+
+        while(leftIndex < rightIndex) {
+            // !(temp[leftIndex] >= 48 && temp[leftIndex] <= 57) || 
+            // !(temp[leftIndex] >= 65 && temp[leftIndex] <= 90) || 
+            // !(temp[leftIndex] >= 97 && temp[leftIndex] <= 122)
+            if (!((temp[leftIndex] >= 48 && temp[leftIndex] <= 57) || (temp[leftIndex] >= 65 && temp[leftIndex] <= 90) || (temp[leftIndex] >= 97 && temp[leftIndex] <= 122))) {
+                leftIndex += 1;
                 continue;
             }
 
-            // Convert it to ascii first
-            // Not in (!)
-            // (charArr[rightPtr] >= 48 && charArr[rightPtr] <= 57) 
-            // || (charArr[rightPtr] >= 65 && charArr[rightPtr] < =90)
-            // || (charArr[rightPtr] >= 97 && charArr[rightPtr] <= 122)
-            int asciiRightChar = (int) charArr[rightPtr];
-            if(!((asciiRightChar >= 48 && asciiRightChar <= 57) || (asciiRightChar >= 65 && asciiRightChar <= 90) || (asciiRightChar >= 97 && asciiRightChar <= 122))) {
-                rightPtr--;
+            // also for rightIndex
+            // !(temp[rightIndex] >= 48 && temp[rightIndex] <= 57) || 
+            // !(temp[rightIndex] >= 65 && temp[rightIndex] <= 90) || 
+            // !(temp[rightIndex] >= 97 && temp[rightIndex] <= 122)
+            if (!((temp[rightIndex] >= 48 && temp[rightIndex] <= 57) || (temp[rightIndex] >= 65 && temp[rightIndex] <= 90) || (temp[rightIndex] >= 97 && temp[rightIndex] <= 122))) {
+                rightIndex -= 1;
                 continue;
             }
 
-            char leftChar = Character.toLowerCase(charArr[leftPtr]);
-            char rightChar = Character.toLowerCase(charArr[rightPtr]);
-            if(leftChar != rightChar) {
+            if(Character.toLowerCase(temp[leftIndex]) != Character.toLowerCase(temp[rightIndex])) {
                 return false;
             }
 
-            leftPtr++;
-            rightPtr--;
+            leftIndex += 1;
+            rightIndex -= 1;
         }
 
         return true;
