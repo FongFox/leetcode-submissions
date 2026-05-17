db7ea8d2bf9ad7e8d941d57410bb3d3bf6af3b26
Tran Gia Nguyen Phong <107380312+FongFox@users.noreply.github.com>
2026-05-17 13:43:21 +0000

    [LeetCode Sync] Runtime - 2 ms (99.38%), Memory - 46.9 MB (83.02%)

--- my-solutions/0001-two-sum/solution.java
+++ my-solutions/0001-two-sum/solution.java
@@ -1,31 +1,15 @@
 class Solution {
-    // Brutal force
-    // public int[] twoSum(int[] nums, int target) {
-    //     int n = nums.length;
-    //     for(int i=0; i<n; i++) {
-    //         int numFind = target - nums[i];
-    //         for(int j=i+1; j<n; j++) {
-    //             if(numFind == nums[j]) {
-    //                 return new int[] {i,j};
-    //             }
-    //         }
-    //     }
-
-    //     return null;
-    // }
-
     public int[] twoSum(int[] nums, int target) {
-       HashMap<Integer, Integer> tempHashMap = new HashMap<Integer, Integer>();
-       
-       int n = nums.length;
-       for(int i=0; i<n; i++) {
-            int numFinding = target - nums[i];
-            if(tempHashMap.containsKey(numFinding)) {
-                return new int[] {i, tempHashMap.get(numFinding)};
-            }
-            tempHashMap.put(nums[i], i);
-       }
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
 
-       return null;
+        for(int i=0; i<nums.length; i++) {
+            int numAvailable = target - nums[i];
+            if(hashMap.containsKey(numAvailable)) {
+                return new int[]{i, hashMap.get(numAvailable)};
+            }
+            hashMap.put(nums[i], i);
+        }
+
+        return new int[2];
     }
 }
